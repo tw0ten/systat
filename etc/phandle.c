8a19fb1a07b7693f9cbe30f0441b4f775a5a1da8
tw0ten <twoten.eq@gmail.com>
2024-11-27 16:42:24 +0200

    t!

--- etc/phandle.c
+++ etc/phandle.c
@@ -1,11 +1,8 @@
-#include <limits.h>
 #include <sys/prctl.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-  prctl(PR_SET_NAME, argv[argc - 1], 0, 0, 0);
-  while (1) {
-    sleep(UINT_MAX);
-  }
-  return 0;
+	prctl(PR_SET_NAME, argv[argc - 1], 0, 0, 0);
+	while (1)
+		sleep(-1);
 }
